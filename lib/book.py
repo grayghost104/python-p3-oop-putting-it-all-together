cbbe36ff70779a57a717d6c51078f283873c571f
Gray Hanna <ghostblossom54288@gmail.com>
2024-07-18 16:10:43 -0600

    complete

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,34 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count, 
+    # turn_page
+    ):
+        self.title = title
+        self.page_count = page_count
+        # self.turn_page = turn_page
+
+    # turn_page = "Flipping the page...wow, you read fast!"
+
+    def get_page(self):
+        return self._page_count
+    def set_page(self, value):
+        if type(value) is int:
+            self._page_count = value
+        else:
+            print("page_count must be an integer")    
+    page_count = property(get_page, set_page)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    # turn_page(Book)
+
+    # def get_turn(self):
+    #     return self._turn_page
+    # def set_turn(self, value):
+    #     if type(value) is str:
+    #         self._turn_page = value
+    #         print("Flipping the page...wow, you read fast!")
+       
+    # turn_page= property(get_turn, set_turn)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,41 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size, 
+    # condition
+    ):
+        self.brand = brand
+        self.size = size
+        self.condition = None
+    
+    def get_size(self):
+        return self._size
+    def set_size(self, value):
+        if type(value) is int:
+            self._size = value
+        else:
+            print("size must be an integer")  
+    size = property(get_size, set_size)
+
+    def cobble(self, 
+    # condition
+    ):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+    # def condition(self):
+    #     condition = "New"
+
+
+
+
+
+
+
+
+
+
+    # @classmethod
+    # def srepair(cls):
+    #     for shoe in cls.all:
+    #         srepair.repair()
